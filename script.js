995c784aa48f351c6d83e0491d876dda7ec95254
FelizardoCaculo <felizardo1987@gmail.com>
2023-12-13 20:13:34 +0100

    uploading project files to github

---
+++ script.js
@@ -0,0 +1,20 @@
+let firstNumber = Number(prompt('Escreva o 1º Número: '));
+let secondNumber = Number(prompt('Escreva o 2º Número: '));
+document.getElementById('firstInputNumber').innerHTML = `1º Número: ${firstNumber}`;
+document.getElementById('secondInputNumber').innerHTML = `2º Número: ${secondNumber}`;
+function sum(){
+    let sum = firstNumber + secondNumber;
+    document.getElementById('calc').innerHTML = `${firstNumber} + ${secondNumber} = ${sum}`;
+}
+function subtration(){
+    let subtration = firstNumber - secondNumber;
+    document.getElementById('calc').innerHTML = `${firstNumber} - ${secondNumber} = ${subtration}`;
+}
+function multiplication(){
+    let multiplication = firstNumber * secondNumber;
+    document.getElementById('calc').innerHTML = `${firstNumber} x ${secondNumber} = ${multiplication}`;
+}
+function division(){
+    let division = firstNumber / secondNumber;
+    document.getElementById('calc').innerHTML = `${firstNumber} / ${secondNumber} = ${division}`;
+}
