6ec9910c88f237015fcdd52c5201045ef2f3229a
FelizardoCaculo <felizardo1987@gmail.com>
2023-12-14 12:17:24 +0100

    changing output results text

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ document.getElementById('firstInputNumber').innerHTML = `1º Número: ${firstNum
 document.getElementById('secondInputNumber').innerHTML = `2º Número: ${secondNumber}`;
 function sum(){
     let sum = firstNumber + secondNumber;
-    document.getElementById('calc').innerHTML = `${firstNumber} + ${secondNumber} = ${sum}`;
+    document.getElementById('calc').innerHTML = `Romando: ${firstNumber} + ${secondNumber} = ${sum}`;
 }
 function subtration(){
     let subtration = firstNumber - secondNumber;
-    document.getElementById('calc').innerHTML = `${firstNumber} - ${secondNumber} = ${subtration}`;
+    document.getElementById('calc').innerHTML = `Subtraindo: ${firstNumber} - ${secondNumber} = ${subtration}`;
 }
 function multiplication(){
     let multiplication = firstNumber * secondNumber;
-    document.getElementById('calc').innerHTML = `${firstNumber} x ${secondNumber} = ${multiplication}`;
+    document.getElementById('calc').innerHTML = `Multiplicando: ${firstNumber} x ${secondNumber} = ${multiplication}`;
 }
 function division(){
     let division = firstNumber / secondNumber;
-    document.getElementById('calc').innerHTML = `${firstNumber} / ${secondNumber} = ${division}`;
+    document.getElementById('calc').innerHTML = `Dividindo: ${firstNumber} / ${secondNumber} = ${division}`;
 }
